8228a1b33f28aa07dcbf8a60a3e5725248bb2237
Brap098 <Brapagon.Work@gmail.com>
2022-10-03 21:45:45 -0400

    HW 4

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -6,11 +6,13 @@ export const CurrentUser = createContext()
 __
 function CurrentUserProvider({ children }) {
 
-    const [currentUser, setCurrentUser] = useState(null)
     useEffect(() => {
-
         const getLoggedInUser = async () => {
-            let response = await fetch('http://localhost:5000/authentication/profile')
+            let response = await fetch('http://localhost:5000/authentication/profile', {
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
+            })
             let user = await response.json()
             setCurrentUser(user)
         }
